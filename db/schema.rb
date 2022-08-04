a5c090cc0b1c9fabed771f5fff2950318a9c289d
Syed Sohail-Nuware <syed.sohail@nuware.com>
2022-08-04 15:16:55 +0530

    Associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_27_101343) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_04_094004) do
   create_table "orders", force: :cascade do |t|
     t.string "product"
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -26,4 +28,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_27_101343) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "orders", "users"
 end
